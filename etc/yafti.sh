60331fc7768e9691fc511d01a0376c43352f6bbb
vatsjijj <29730787+vatsjijj@users.noreply.github.com>
2025-10-13 13:15:34 -0400

    Send notification in YAFTI autostart
    let the user know that yafti is about to launch

--- etc/yafti.sh
+++ etc/yafti.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+notify-send -u normal -t 5000 -a "System" "Hello, welcome to Slate!" "Please wait for the first boot installer to launch."
+
 AUTOSTART_FILE="$HOME/.config/autostart/yafti-first-boot.desktop"
 
 /usr/bin/yafti /etc/yafti/yafti.yml
